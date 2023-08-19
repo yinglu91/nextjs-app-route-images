91103b5197e7856000627c3c35fb6a6e51068329
Ying Lu <yinglu5374@gmail.com>
2023-08-19 08:16:11 -0400

    add ReactQueryDevtools

--- src/app/ReactQueryProviderWrapper.tsx
+++ src/app/ReactQueryProviderWrapper.tsx
@@ -2,12 +2,13 @@
  
 import React, {ReactNode} from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const queryClientParams = {
     defaultOptions: {
         queries: {
-            staleTime: 0*60*1000,
-            cacheTime: 0*60*1000,
+            staleTime: 1*60*1000,
+            cacheTime: 1*60*1000,
             retry: false,
             refetchOnWindowFocus: false,
             refetchOnReconnect: false,
@@ -18,10 +19,12 @@ export const queryClientParams = {
 export const queryClient = new QueryClient(queryClientParams)
  
 type Props = {children: ReactNode}
+
 export function QueryClientProviderWrapper({children}: Props) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
